5792611e6a0d0524f28754897a38f21b57cc84c9
aandrade1980 <varito1@gmail.com>
2019-04-15 12:26:24 -0700

    Updated scss. Modified jobItem ui

--- src/components/JobItem.js
+++ src/components/JobItem.js
@@ -5,12 +5,11 @@ import './JobItem.scss';
 
 export default function JobItem({ jobId, title, description, imgUrl, createdAt, deleteJob }) {
   const createdAtDate =  new Date(createdAt);
-  console.log("createdAtDate => ", createdAtDate);
   
   return (
     <div className="jobCard">
       <button type="button" className="btn btn-danger" style={ styles.button } onClick={ () => deleteJob(jobId) }>Delete</button>
-      <h4>{ title }</h4>
+      <h5>{ title }</h5>
       <p style={ styles.p }>{ description }</p>
       <img style={ styles.img } alt="Job" src={ imgUrl } />
       <span>Upload on: { createdAtDate.toDateString() }</span>
