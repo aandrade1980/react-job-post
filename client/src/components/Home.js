c526f9371191e4566d45e5128d902dee19ef49a0
aandrade1980 <varito1@gmail.com>
2019-05-06 20:47:38 -0300

    Updated login and sing up forms

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,8 +4,14 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className="d-flex justify-content-center m-top-125">
-      <Link className="links" to="/login">Login</Link>
-      <Link className="links" to="/singup">SignUp</Link>
+      <button type="button" className="btn btn-outline-dark btn-lg mr-5">
+        <i className="fas fa-sign-in-alt mr-1"></i>
+        <Link className="links" to="/login">Login</Link>
+      </button>
+      <button type="button" className="btn btn-outline-dark btn-lg">
+        <i className="fas fa-user-plus mr-1"></i>
+        <Link className="links" to="/singup">Sign Up</Link>
+      </button>
     </div>
   )
 }
