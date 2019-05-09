15a07cf59c267fe1b4f3b1f390ae1824e3508dab
aandrade1980 <varito1@gmail.com>
2019-05-09 19:11:54 -0300

    Fixed user img bug

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -15,7 +15,7 @@ const Header = ({ user, title, history }) => {
             </h1>
             { user && user.displayName &&
               <button className="btn btn-outline-light mr-2" onClick={ evt => value.logOut(evt, history) }>
-                <img className="mr-2" style={{ height: '40px', borderRadius: '50%' }} src={ user.photoURL } alt="User" />
+                { user && user.photoURL && <img className="mr-2" style={{ height: '40px', borderRadius: '50%' }} src={ user.photoURL } alt="User" /> }
                 <span>
                   { user && !user.photoURL && <i className="far fa-user mr-2"></i> }
                   { user && user.displayName }
