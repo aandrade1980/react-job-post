5cccb468c5f30f30114e2ce547019f117763de43
aandrade1980 <varito1@gmail.com>
2019-05-09 23:01:41 -0300

    Updated login providers buttons

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -29,20 +29,18 @@ function Login({ history }) {
             }
             <form style={{ minWidth: '450px', margin: '4rem auto' }} className="form w-25" onSubmit={ evt => value.logIn(evt, formData.email, formData.password, history) }>
             <h3 className="mb-3 text-center">Login</h3>
-            <div className="mb-4">
+            <div className="mb-4 mt-4 d-flex justify-content-center">
               <button 
                   onClick={ () => value.providerSignIn(GOOGLE_PROVIDER, history) }
-                  className="btn btn-outline-primary btn-block mr-2 mt-4"
+                  className="btn btn-outline-primary provider-login-button mr-5"
                 >
                   <i className="fab fa-google mr-2"></i>
-                  Login with Google
                 </button>
                 <button 
                   onClick={ () => value.providerSignIn(GITHUB_PROVIDER, history) }
-                  className="btn btn-outline-dark btn-block mb-3"
+                  className="btn btn-outline-dark provider-login-button"
                 >
                   <i className="fab fa-github-alt mr-2"></i>
-                  Login with Github
                 </button>
             </div>
             <hr/>
