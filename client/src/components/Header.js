560d44da04ea00d809f88f8d720f8b9307575992
aandrade1980 <varito1@gmail.com>
2019-07-03 00:29:16 -0300

    Added new post and eslint rules

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,32 +1,45 @@
-import * as React from 'react';
-import { UserConsumer } from '../context';
+import * as React from "react";
+import PropTypes from "prop-types";
+import { UserConsumer } from "../context";
 
-import './Header.scss';
+import "./Header.scss";
 
 const Header = ({ title }) => {
   return (
     <UserConsumer>
-      { value => {
+      {value => {
         const { user, logOut } = value;
         return (
           <header className="header">
-            <h1 className="flex-grow-1 text-center">
-              { title }
-            </h1>
-            { user &&
-              <button className="btn btn-outline-light mr-2" onClick={ evt => logOut(evt) }>
-                { user.photoURL && <img className="mr-2" style={{ height: '40px', borderRadius: '50%' }} src={ user.photoURL } alt="User" /> }
+            <h1 className="flex-grow-1 text-center">{title}</h1>
+            {user && (
+              <button
+                className="btn btn-outline-light mr-2"
+                onClick={evt => logOut(evt)}
+              >
+                {user.photoURL && (
+                  <img
+                    className="mr-2"
+                    style={{ height: "40px", borderRadius: "50%" }}
+                    src={user.photoURL}
+                    alt="User"
+                  />
+                )}
                 <span>
-                  { !user.photoURL && <i className="far fa-user mr-2"></i> }
-                  { user.displayName }
+                  {!user.photoURL && <i className="far fa-user mr-2" />}
+                  {user.displayName}
                 </span>
               </button>
-            }
+            )}
           </header>
-        )
+        );
       }}
     </UserConsumer>
-  )
+  );
+};
+
+Header.propTypes = {
+  title: PropTypes.string
 };
 
 export default React.memo(Header);
--- client/src/components/JobPost.js
+++ client/src/components/JobPost.js
@@ -1,56 +1,73 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 
 const JobPost = ({ match, history }) => {
-
   const [job, setJob] = useState({});
 
   useEffect(() => {
     let isSubscribed = true;
     const { jobId } = match.params;
-    
-    jobId && fetch(`/api/getJob/${jobId}`)
-      .then(response => response.json())
-      .then(jsonResponse => {
-        isSubscribed && 
-        setJob({
-          jobId,
-          title: jsonResponse.job.title,
-          description: jsonResponse.job.description,
-          imgUrl: jsonResponse.job.imgUrl,
-          createdAt: new Date(jsonResponse.job.createdAt),
-          email: jsonResponse.job.email,
-          company: jsonResponse.job.company,
-          category: jsonResponse.job.category
-        })}
-      )
-    return () => isSubscribed = false;
+
+    jobId &&
+      fetch(`/api/getJob/${jobId}`)
+        .then(response => response.json())
+        .then(jsonResponse => {
+          isSubscribed &&
+            setJob({
+              jobId,
+              title: jsonResponse.job.title,
+              description: jsonResponse.job.description,
+              imgUrl: jsonResponse.job.imgUrl,
+              createdAt: new Date(jsonResponse.job.createdAt),
+              email: jsonResponse.job.email,
+              company: jsonResponse.job.company,
+              category: jsonResponse.job.category
+            });
+        });
+    return () => (isSubscribed = false);
   }, [match.params]);
 
   const openEditForm = () => {
     const path = `/updateJob/${job.jobId}`;
-      history.push(path);
-  }
+    history.push(path);
+  };
 
   return (
     <div className="jobContainer d-flex m-top-25 justify-content-center alg-items-center">
-      { job.imgUrl && <img className="wth-55 height-55 m-right-15 max-wth-650" alt="Job" src={ `/${job.imgUrl}` } /> }
+      {job.imgUrl && (
+        <img
+          className="wth-55 height-55 m-right-15 max-wth-650"
+          alt="Job"
+          src={`/${job.imgUrl}`}
+        />
+      )}
       <div className="jobInfo m-right-15 max-wth-33">
-        <h4>{ job.title }</h4>
-        <h5>{ job.category }</h5>
-        <h6>{ job.email }</h6>
-        <h6>{ job.company }</h6>
-        <p className="white-space-pw">{ job.description }</p>
-        <h6>{ job.createdAt && job.createdAt.toDateString() }</h6>
+        <h4>{job.title}</h4>
+        <h5>{job.category}</h5>
+        <h6>{job.email}</h6>
+        <h6>{job.company}</h6>
+        <p className="white-space-pw">{job.description}</p>
+        <h6>{job.createdAt && job.createdAt.toDateString()}</h6>
       </div>
       <div className="jobEditButtonContainer">
-        { job.title && 
-          <button type="button" className="btn btn-outline-primary btn-sm" onClick={ openEditForm }>
-            Edit<i className="far fa-edit m-left-5"></i>
+        {job.title && (
+          <button
+            type="button"
+            className="btn btn-outline-primary btn-sm"
+            onClick={openEditForm}
+          >
+            Edit
+            <i className="far fa-edit m-left-5" />
           </button>
-        }
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
+
+JobPost.propTypes = {
+  match: PropTypes.object,
+  history: PropTypes.object
+};
 
 export default React.memo(JobPost);
