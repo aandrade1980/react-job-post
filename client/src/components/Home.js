1107f45bcccfb93531e247477b701e45eb6add00
aandrade1980 <varito1@gmail.com>
2019-08-05 16:17:57 -0300

    Lint home js file

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,50 +1,51 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense } from "react";
 
-import { UserConsumer } from '../context';
-import { FORM_LOGIN, FORM_SING_UP } from '../utilities/constants';
+import { UserConsumer } from "../context";
+import { FORM_LOGIN, FORM_SING_UP } from "../utilities/constants";
 
-import Spinner from './Spinner';
+import Spinner from "./Spinner";
 
-const Alert = lazy(() => import('./Alert'));
-const Modal = lazy(() => import('./Modal'));
+const Alert = lazy(() => import("./Alert"));
+const Modal = lazy(() => import("./Modal"));
 
 const Home = () => {
   return (
     <UserConsumer>
-      { value => {
-        const { error, setError, setOpenModal } = value; 
+      {value => {
+        const { error, setError, setOpenModal } = value;
         return (
           <>
-            { error &&
-              <Suspense fallback={ <Spinner /> }>
-                <Alert error={ error } setError={ setError } /> 
+            {error && (
+              <Suspense fallback={<Spinner />}>
+                <Alert error={error} setError={setError} />
               </Suspense>
-            }
-            <Suspense fallback={ <Spinner /> }>
+            )}
+            <Suspense fallback={<Spinner />}>
               <Modal />
             </Suspense>
             <div className="d-flex justify-content-center m-top-125">
-              <button 
-                type="button" 
-                className="btn btn-outline-dark btn-lg mr-5" 
-                onClick={ () => setOpenModal({ show: true, form: FORM_LOGIN }) }>
-                <i className="fas fa-sign-in-alt mr-1"></i>
+              <button
+                type="button"
+                className="btn btn-outline-dark btn-lg mr-5"
+                onClick={() => setOpenModal({ show: true, form: FORM_LOGIN })}
+              >
+                <i className="fas fa-sign-in-alt mr-1" />
                 Login
               </button>
-              <button 
-                type="button" 
+              <button
+                type="button"
                 className="btn btn-outline-dark btn-lg"
-                onClick={ () => setOpenModal({ show: true, form: FORM_SING_UP }) }
-                >
-                <i className="fas fa-user-plus mr-1"></i>
+                onClick={() => setOpenModal({ show: true, form: FORM_SING_UP })}
+              >
+                <i className="fas fa-user-plus mr-1" />
                 Sign Up
               </button>
             </div>
           </>
-        )}
-      }
+        );
+      }}
     </UserConsumer>
-  )
-}
+  );
+};
 
 export default Home;
